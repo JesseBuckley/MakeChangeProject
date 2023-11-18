40781b8636f659325d5c29c921077ca2cfdb0ee6
JesseBuckley <jessebuckley2@gmail.com>
2023-11-18 02:22:01 -0500

    Almost finished with project just have to work the comma/period problem out.

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # MakeChangeProject
 
 # How to Run
+1. Run in terminal.
+You'll be prompted to enter the price of your item.
+
+2. Enter the price into terminal.
+You'll then be prompted to give the cashier an amount of money to pay for the item you selected.
+
+3. Which then returns how much money you gave the cashier, how much total your change is. And then your change based on type from highest to lowest.
 
 # Project Description
-
+Make Change Project is a program that acts like a cash register. 
+You bring a item to the register, you produce money to pay for said item.
+If you only have larger bills that is fine too. The register has plenty of change.
 # Technologies Used
-
+-Java
+-Eclispe
+-Google (extensively)
 # Lessons Learned
+ -I have somewhat learned when to use % statements.
+-Reiterated my understanding of casting data types a bit.
+-I have a very weak understanding of the "Math." function so I spent many hours trying to find a way to round up the numbers before I just started Googling. To come to find that Math.round saves lives. 
--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -11,31 +11,79 @@ public class CashRegister {
 //The user is then *prompted* asking how much money was *tendered* by the customer.
 		System.out.println("How much money do you give the cashier to pay for this item?");
 		double tendered = sc.nextDouble();
-		int divisible = 1;
 		double change = tendered - costOfItem;
-//		double usedCoins = 0; double needDollars = 0; 
-		double penny;
-		double nickle;
-		double dime;
-		double quarter;
-		double dollar;
-		double fiveDollar;
-		double tenDollar;
-		double twentyDollar;
+		sc.close();
 
 //Display an appropriate message if the customer provided too little money or the exact amount.
 		if (tendered < costOfItem) {
-			System.out.println("This is less than what you need to pay, I cannot accept this, have a nice day...");
+			System.err.println("This is less than what you need to pay, I cannot accept this.");
 		} else if (costOfItem == tendered) {
 			System.out.println("Looks like you're all set then! Thank you have a nice day!");
 		}
 		if (tendered > costOfItem) {
-			System.out.println("You have given me " + tendered + " your change is " + change);
-			
-			
-				
-				}
+			System.out.println("You gave me " + tendered + ", your change is " + change + ".");
+
+			// turn change into whole number somehow, times it by 100 move decimal point to
+			// represent that
+			int changeInt = (int) Math.round(change * 100);
+			// internet has shown me the light i have rounded the number up and * 100 to
+			// account for pennies
+			// ints to represent the new values if you move them over two places
+			int penny = 1;
+			int nickel = 5;
+			int dime = 10;
+			int quarter = 25;
+			int dollar = 100;
+			int fiveDollar = 500;
+			int tenDollar = 1000;
+			int twentyDollar = 2000;
+
+			// int howmany = the int version of change divided by the new versions of the
+			// dollars and coins
+
+			int howManyTwenty = changeInt / twentyDollar;
+			changeInt = changeInt % twentyDollar;
+			if (howManyTwenty >= 1) {
+				System.out.print(howManyTwenty + " twenty dollar bill, ");
+			}
+			int howManyTen = changeInt / tenDollar;
+			changeInt = changeInt % tenDollar;
+			if (howManyTen >= 1) {
+				System.out.print(howManyTen + " ten dollar bill, ");
+			}
+			int howManyFive = changeInt / fiveDollar;
+			changeInt = changeInt % fiveDollar;
+			if (howManyFive >= 1) {
+				System.out.print(howManyFive + " five dollar bill, ");
+			}
+			int howManyOne = changeInt / dollar;
+			changeInt = changeInt % dollar;
+			if (howManyOne >= 1) {
+				System.out.print(howManyOne + " one dollar bill, ");
+			}
+			int howManyQuarter = changeInt / quarter;
+			changeInt = changeInt % quarter;
+			if (howManyQuarter >= 1) {
+				System.out.print(howManyQuarter + " quarter, ");
+			}
+			int howManyDime = changeInt / dime;
+			changeInt = changeInt % dime;
+			if (howManyDime >= 1) {
+				System.out.print(howManyDime + " dime, ");
+			}
+			int howManyNickel = changeInt / nickel;
+			changeInt = changeInt % nickel;
+			if (howManyNickel >= 1) {
+				System.out.print(howManyNickel + " nickel, ");
+			}
+			int howManyPenny = changeInt / penny;
+			changeInt = changeInt % penny;
+			if (howManyPenny == 1) {
+				System.out.print(howManyPenny + " penny. ");
+			} else if (howManyPenny > 1) {
+				System.out.println(howManyPenny + " pennies. ");
+				// to do commas add && statements to every one to check if pennys are used.
 			}
 		}
-	
-
+	}
+}
