edaeb68c54338a9bcca41e4d07dc491cb1643c5d
JesseBuckley <jessebuckley2@gmail.com>
2023-11-17 21:44:43 -0500

    working on the hard part of assignment

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -1,5 +1,41 @@
 package makechange;
 
-public class CashRegister {
+import java.util.Scanner;
 
-}
+public class CashRegister {
+	public static void main(String[] args) {
+		// The user is *prompted* asking for the *price* of the *item*.
+		Scanner sc = new Scanner(System.in);
+		System.out.println("What is the price of your item?");
+		double costOfItem = sc.nextDouble();
+//The user is then *prompted* asking how much money was *tendered* by the customer.
+		System.out.println("How much money do you give the cashier to pay for this item?");
+		double tendered = sc.nextDouble();
+		int divisible = 1;
+		double change = tendered - costOfItem;
+//		double usedCoins = 0; double needDollars = 0; 
+		double penny;
+		double nickle;
+		double dime;
+		double quarter;
+		double dollar;
+		double fiveDollar;
+		double tenDollar;
+		double twentyDollar;
+
+//Display an appropriate message if the customer provided too little money or the exact amount.
+		if (tendered < costOfItem) {
+			System.out.println("This is less than what you need to pay, I cannot accept this, have a nice day...");
+		} else if (costOfItem == tendered) {
+			System.out.println("Looks like you're all set then! Thank you have a nice day!");
+		}
+		if (tendered > costOfItem) {
+			System.out.println("You have given me " + tendered + " your change is " + change);
+			
+			
+				
+				}
+			}
+		}
+	
+
