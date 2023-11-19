26096afeaf3c22eecd0de62374f526b340cd9221
JesseBuckley <jessebuckley2@gmail.com>
2023-11-18 19:25:13 -0500

    Cash Register Project final

--- README.md
+++ README.md
@@ -18,6 +18,10 @@ If you only have larger bills that is fine too. The register has plenty of chang
 -Eclispe
 -Google (extensively)
 # Lessons Learned
+-I am completely new to coding and I need lots of practice on all these newly learned features and functions.
+-I was defintely a little shaky on my grasp of (if, and if else statements). I feel much more confident with them after this project.
  -I have somewhat learned when to use % statements.
 -Reiterated my understanding of casting data types a bit.
--I have a very weak understanding of the "Math." function so I spent many hours trying to find a way to round up the numbers before I just started Googling. To come to find that Math.round saves lives. 
+-I have a very weak understanding of the "Math." function so I spent many hours trying to find a way to round up the numbers before I just started Googling. To come to find that Math.round times 100 would round my double number to a whole one. Which made the logic of getting each coin and bill alot easier for me to grasp. 
+-I know the homework did not specify to do this. But I learned to make each possible output have the logic to know when to use a comma or period. 
+- I decided to try and throw the logic that calculates change into its own method. I got that to work which was great. Makes my code look much less messy.
--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -18,72 +18,139 @@ public class CashRegister {
 		if (tendered < costOfItem) {
 			System.err.println("This is less than what you need to pay, I cannot accept this.");
 		} else if (costOfItem == tendered) {
-			System.out.println("Looks like you're all set then! Thank you have a nice day!");
+			System.out.println("Looks like you're all set then! Thank you, have a nice day!");
 		}
 		if (tendered > costOfItem) {
-			System.out.println("You gave me " + tendered + ", your change is " + change + ".");
-
-			// turn change into whole number somehow, times it by 100 move decimal point to
-			// represent that
-			int changeInt = (int) Math.round(change * 100);
-			// internet has shown me the light i have rounded the number up and * 100 to
-			// account for pennies
-			// ints to represent the new values if you move them over two places
-			int penny = 1;
-			int nickel = 5;
-			int dime = 10;
-			int quarter = 25;
-			int dollar = 100;
-			int fiveDollar = 500;
-			int tenDollar = 1000;
-			int twentyDollar = 2000;
-
-			// int howmany = the int version of change divided by the new versions of the
-			// dollars and coins
-
-			int howManyTwenty = changeInt / twentyDollar;
-			changeInt = changeInt % twentyDollar;
-			if (howManyTwenty >= 1) {
-				System.out.print(howManyTwenty + " twenty dollar bill, ");
-			}
-			int howManyTen = changeInt / tenDollar;
-			changeInt = changeInt % tenDollar;
-			if (howManyTen >= 1) {
-				System.out.print(howManyTen + " ten dollar bill, ");
-			}
-			int howManyFive = changeInt / fiveDollar;
-			changeInt = changeInt % fiveDollar;
-			if (howManyFive >= 1) {
-				System.out.print(howManyFive + " five dollar bill, ");
-			}
-			int howManyOne = changeInt / dollar;
-			changeInt = changeInt % dollar;
-			if (howManyOne >= 1) {
-				System.out.print(howManyOne + " one dollar bill, ");
-			}
-			int howManyQuarter = changeInt / quarter;
-			changeInt = changeInt % quarter;
-			if (howManyQuarter >= 1) {
-				System.out.print(howManyQuarter + " quarter, ");
-			}
-			int howManyDime = changeInt / dime;
-			changeInt = changeInt % dime;
-			if (howManyDime >= 1) {
-				System.out.print(howManyDime + " dime, ");
-			}
-			int howManyNickel = changeInt / nickel;
-			changeInt = changeInt % nickel;
-			if (howManyNickel >= 1) {
-				System.out.print(howManyNickel + " nickel, ");
-			}
-			int howManyPenny = changeInt / penny;
-			changeInt = changeInt % penny;
-			if (howManyPenny == 1) {
-				System.out.print(howManyPenny + " penny. ");
-			} else if (howManyPenny > 1) {
-				System.out.println(howManyPenny + " pennies. ");
-				// to do commas add && statements to every one to check if pennys are used.
-			}
+			calculateChange(change, tendered, costOfItem);
+//interesting struggle to print commas and periods in the right places when i wanted them. finally got it though.
 		}
 	}
+	
+	
+	
+	public static void calculateChange(double change, double tendered, double costOfItem) {
+		
+		int changeInt = (int) Math.round(change * 100);
+		
+		change = (float) change;
+		int penny = 1; int nickel = 5; int dime = 10; int quarter = 25;
+		int dollar = 100; int fiveDollar = 500; int tenDollar = 1000; int twentyDollar = 2000;
+		
+		// int howmany = the int version of change divided by the new versions of the
+		// dollars and coins
+		
+		int howManyTwenty = changeInt / twentyDollar;
+		changeInt = changeInt % twentyDollar;
+		if (howManyTwenty > 1 && twentyDollar % 2000 == changeInt) {
+			System.out.print(howManyTwenty + " twenty dollar bills");
+			howManyTwenty = 0;
+		} if (howManyTwenty >= 1 && twentyDollar % 2000 == changeInt) {
+				System.out.print(howManyTwenty + " twenty dollar bill");
+				howManyTwenty = 0;
+		} if (howManyTwenty > 1) {
+			System.out.print(howManyTwenty + " twenty dollar bills, ");
+		} else if (howManyTwenty >= 1) {
+			System.out.print(howManyTwenty + " twenty dollar bill, ");
+		}
+		
+		int howManyTen = changeInt / tenDollar;
+		changeInt = changeInt % tenDollar;
+		if (howManyTen > 1 && tenDollar % 1000 == changeInt) {
+			System.out.print(howManyTen + " ten dollar bills");
+			howManyTen = 0;
+		} if (howManyTen >= 1 && tenDollar % 1000 == changeInt) {
+				System.out.print(howManyTen + " ten dollar bill");
+				howManyTen = 0;
+		} if (howManyTen > 1) {
+			System.out.print(howManyTen + " ten dollar bills, ");
+		} else if (howManyTen >= 1) {
+			System.out.print(howManyTen + " ten dollar bill, ");
+		}
+		
+		int howManyFive = changeInt / fiveDollar;
+		changeInt = changeInt % fiveDollar;
+		
+		if (howManyFive > 1 && fiveDollar % 500 == changeInt) {
+			System.out.print(howManyFive + " five dollar bill");
+			howManyFive = 0;
+		} if (howManyFive >= 1 && fiveDollar % 500 == changeInt) {
+				System.out.print(howManyFive + " five dollar bill");
+				howManyFive = 0;
+		} if (howManyFive > 1) {
+			System.out.print(howManyFive + " five dollar bills, ");
+		} else if (howManyFive >= 1) {
+			System.out.print(howManyFive + " five dollar bill, ");
+		}
+		
+		int howManyOne = changeInt / dollar;
+		changeInt = changeInt % dollar;
+		if (howManyOne > 1 && dollar % 100 == changeInt) {
+			System.out.print(howManyOne + " one dollar bills");
+			howManyOne = 0;	
+		} if (howManyOne >= 1 && dollar % 100 == changeInt) {
+			System.out.print(howManyOne + " one dollar bill");
+			howManyOne = 0;
+		} if (howManyOne > 1) {
+			System.out.print(howManyOne + " one dollar bills, ");
+		} else if (howManyOne >= 1) {
+			System.out.print(howManyOne + " one dollar bill, ");
+		}
+		
+		int howManyQuarter = changeInt / quarter;
+		changeInt = changeInt % quarter;
+		if (howManyQuarter > 1 && quarter % 25 == changeInt) {
+			System.out.print(howManyQuarter + " quarters");
+			howManyQuarter = 0;
+		} if (howManyQuarter >= 1 && quarter % 25 == changeInt) {
+			System.out.print(howManyQuarter + " quarter");
+			howManyQuarter = 0;
+		} if (howManyQuarter > 1) {
+			System.out.print(howManyQuarter + " quarters, ");
+		} else if (howManyQuarter >= 1) {
+			System.out.print(howManyQuarter + " quarter, ");
+		}
+		
+		int howManyDime = changeInt / dime;
+		changeInt = changeInt % dime;
+		if (howManyDime > 1 && dime % 10 == changeInt) {
+			System.out.print(howManyDime + " dimes");
+			howManyDime = 0;
+		} if (howManyDime >= 1 && dime % 10 == changeInt) {
+			System.out.print(howManyDime + " dime");
+			howManyDime = 0;
+		} if (howManyDime > 1) {
+			System.out.print(howManyDime + " dimes, ");
+		} else if (howManyDime >= 1) {
+			System.out.println(howManyDime + " dime, ");
+		}
+		
+		int howManyNickel = changeInt / nickel;
+		changeInt = changeInt % nickel;
+		if (howManyNickel > 1 && nickel % 5 == changeInt) {
+			System.out.print(howManyNickel + " dimes");
+			howManyNickel = 0;
+		} if (howManyNickel >= 1 && nickel % 5 == changeInt) {
+			System.out.print(howManyNickel + " nickel");
+			howManyNickel = 0;
+		} if (howManyNickel > 1) {
+			System.out.print(howManyNickel + " nickels, ");
+		} else if (howManyNickel >= 1) {
+			System.out.print(howManyNickel + " nickel, ");
+		}
+		int howManyPenny = changeInt / penny;
+		changeInt = changeInt % penny;
+		if (howManyPenny == 1) {
+			System.out.print(howManyPenny + " penny");
+		} else if (howManyPenny > 1) {
+			System.out.print(howManyPenny + " pennies");
+		}
+		System.out.print(".");
+		System.out.print("\n" + "\n" + "-------*Your receipt is below*-------" + "\n"); 
+		System.out.printf("\n" + "--Item price: " + "(" + "%.2f", costOfItem); 
+		System.out.print(")");System.out.printf("\n" + "---You paid: " + "(" + "%.2f", tendered);
+		System.out.println(")"); System.out.printf("--Your change: " + "(" + "%.2f", change); 
+		System.out.println(")" + "\n" + "\n" + "           ---Thank you---");
+	}
 }
+
+
